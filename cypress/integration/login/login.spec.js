a8698de23448181857e8fb575c575c15c48e5efc
sanjanasw <sswitharanage@students.nsbm.ac.lk>
2022-03-31 20:46:22 +0530

    feat:

--- cypress/integration/login/login.spec.js
+++ cypress/integration/login/login.spec.js
@@ -93,4 +93,30 @@ describe("student should be able to login to system using login screen", () => {
       expect(response.body.message).to.eq("Please verify your email!");
     });
   });
+
+  it("users should be able to click login button without filling form", () => {
+    cy.visit("http://localhost:4200/auth/login");
+    cy.intercept("POST", "**/auth/login").as("login");
+
+    cy.get('[data-qa="auth_login_btn_login"]').should("be.disabled");
+
+    cy.get('[data-qa="auth_login_input_username"]').type("testStaff");
+
+    cy.get('[data-qa="auth_login_btn_login"]').should("be.disabled");
+
+    cy.get('[data-qa="auth_login_input_password"]').type(
+      "$NewUserPassword1Temp"
+    );
+
+    cy.get('[data-qa="auth_login_check_remember"]').click();
+
+    cy.get('[data-qa="auth_login_btn_login"]').should("not.be.disabled");
+
+    cy.get('[data-qa="auth_login_btn_login"]').click();
+
+    cy.wait("@login").then(({ response }) => {
+      expect(response.statusCode).to.eq(403);
+      expect(response.body.message).to.eq("Please verify your email!");
+    });
+  });
 });
